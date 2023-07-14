ee4d4c951aa616dfc5ebf4ab3fae9e5c820a0f01
Theodore Smith <tjs7321@gmail.com>
2023-07-14 05:27:12 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const mainToBeRemoved = document.getElementById("main")
+
+mainToBeRemoved.remove()
+
+const newH1 = document.createElement('h1')
+
+newH1.id = "victory"
+
+newH1.textContent = "Teddy is the champion"
+
+document.body.appendChild(newH1)
+
+const newHeader = document.getElementById('victory')
